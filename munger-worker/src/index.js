6c4e726f01680d87bc1e7dfd72f91406fc0dff2e
jinukahn98-lab <jinukahn98@gmail.com>
2026-08-27 22:07:37 +0900

    feat: inject chapter-level deep dive into Munger digest

--- munger-worker/src/index.js
+++ munger-worker/src/index.js
@@ -1,5 +1,15 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+    const type = response.headers.get('content-type') || '';
+    if (!type.includes('text/html')) return response;
+
+    return new HTMLRewriter()
+      .on('body', {
+        element(element) {
+          element.append('<script src="/deep-dive.js?v=20260827-2" defer></script>', { html: true });
+        },
+      })
+      .transform(response);
   },
 };
